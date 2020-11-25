717860e1bc279808bfa7e3e9dbe256f550283ae6
石渡拓海 <t-ishiwata@sight-visit.com>
2020-11-25 11:30:53 +0900

    [Delete] コマンド入力部分が重複していたため削除

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,8 +5,5 @@ module Types
     field :description, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :id, ID, null: false
-    field :title, String, null: false
-    field :description, String, null: false
   end
 end
