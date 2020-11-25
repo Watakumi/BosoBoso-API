edfae249bfb696e62af2a77480caf3f2dfa141ad
石渡拓海 <t-ishiwata@sight-visit.com>
2020-11-25 11:19:48 +0900

    bundle exec rails g graphql:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   namespace :api do
     namespace :v1 do
       resources :posts
     end
+  end
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
