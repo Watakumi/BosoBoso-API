9dae28581dbe2497def87fef537925d5290f5272
石渡拓海 <t-ishiwata@sight-visit.com>
2020-11-25 11:29:27 +0900

    bundle exec rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_123706) do
+ActiveRecord::Schema.define(version: 2020_11_25_022459) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
